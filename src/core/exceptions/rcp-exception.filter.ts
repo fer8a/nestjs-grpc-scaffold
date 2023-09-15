6f938615109ee38650d118908dc8ad2f8458a60d
Fernando Ochoa <ferj8a@gmail.com>
2023-09-15 19:16:27 -0300

    Add logger for bad request exceptions

--- src/core/exceptions/rcp-exception.filter.ts
+++ src/core/exceptions/rcp-exception.filter.ts
@@ -1,9 +1,23 @@
-import { Catch, ArgumentsHost } from '@nestjs/common';
+import {
+  Catch,
+  ArgumentsHost,
+  BadRequestException,
+  Logger,
+} from '@nestjs/common';
 import { BaseRpcExceptionFilter, RpcException } from '@nestjs/microservices';
 
 @Catch()
 export class RcpExceptionsFilter extends BaseRpcExceptionFilter {
+  constructor(private readonly logger: Logger) {
+    super();
+    this.logger = new Logger(RcpExceptionsFilter.name);
+  }
+
   catch(exception: unknown, host: ArgumentsHost) {
+    if (exception instanceof BadRequestException) {
+      this.logger.error(exception.getResponse(), 'BadRequestException');
+    }
+
     return super.catch(new RpcException(<object>exception), host);
   }
 }
