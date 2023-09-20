dd57afc540a0a4c21bf72351f1723599c254f711
Fernando Ochoa <ferj8a@gmail.com>
2023-09-20 19:32:46 -0300

    Add extra dependencies and missing scripts

--- src/main.config.ts
+++ src/main.config.ts
@@ -8,10 +8,10 @@ import { GrpcOptions, Transport } from '@nestjs/microservices';
 export const mainConfig: GrpcOptions = {
   transport: Transport.GRPC,
   options: {
-    package: ['base', 'health'],
+    package: ['health', 'base'],
     protoPath: [
-      './src/resources/base/proto/base.proto',
       './src/resources/healthcheck/proto/health.proto',
+      './src/resources/base/proto/base.proto',
     ],
     url: `${process.env.HOST || 'localhost'}:${process.env.PORT || '5000'}`,
   },
